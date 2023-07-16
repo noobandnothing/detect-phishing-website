aef3ba3028ed95ae9d7bd0af8097c36e4d127dc5
noobandnothing <popcorn.server.project@gmail.com>
2023-07-17 02:10:08 +0300

    DTC : create model
    Co-authored-by: Muhammed Amr <96207542+Muhamedamr2001@users.noreply.github.com>

--- DTC.py
+++ DTC.py
@@ -15,3 +15,7 @@ relations = relations.sort_values(by=['class'],ascending=False)
 X = X[relations[relations['class'] > 0.09].index]
 ##################################################
 X_train, X_test , y_train, y_test = train_test_split(X, y, test_size = 0.2 ,random_state = 42)
+
+
+clf = DecisionTreeClassifier(criterion="entropy")
+clf = clf.fit(X_train, y_train)
