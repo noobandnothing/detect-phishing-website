28da49c59e8713b4c6c79fc2033762bec102b825
noobandnothing <popcorn.server.project@gmail.com>
2023-07-17 02:24:00 +0300

    DTC : Get ALL POSSIBLE TREES with different ccp_alpha and add some plots

--- DTC.py
+++ DTC.py
@@ -50,3 +50,27 @@ def ccp_alphas_VS_impurities():
     ax.set_title("Total Impurity vs effective alpha for training set")
 
 ccp_alphas_VS_impurities()
+
+##################### Get ALL POSSIBLE TREES ######################
+clfs = []
+for ccp_alpha in ccp_alphas:
+    clf = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
+    clf.fit(X_train, y_train)
+    clfs.append(clf)
+######################### SOME PLOTTING ###########################
+
+def nodes_depth_VS_aplha():
+    node_counts = [clf.tree_.node_count for clf in clfs]
+    depth = [clf.tree_.max_depth for clf in clfs]
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(ccp_alphas, node_counts, marker="o", drawstyle="steps-post")
+    ax[0].set_xlabel("alpha")
+    ax[0].set_ylabel("number of nodes")
+    ax[0].set_title("Number of nodes vs alpha")
+    ax[1].plot(ccp_alphas, depth, marker="o", drawstyle="steps-post")
+    ax[1].set_xlabel("alpha")
+    ax[1].set_ylabel("depth of tree")
+    ax[1].set_title("Depth vs alpha")
+    fig.tight_layout()
+    
+nodes_depth_VS_aplha()
