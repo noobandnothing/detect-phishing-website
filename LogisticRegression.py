1056c97db80f1d9849fbdef2bfa04eb5d880b5c7
noobandnothing <popcorn.server.project@gmail.com>
2023-07-17 01:51:33 +0300

    LOG_REG : Split data to train and test
    Co-authored-by: Muhammed Amr <96207542+Muhamedamr2001@users.noreply.github.com>

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -19,3 +19,11 @@ y = df.iloc[0: , 30]
 relations = df.corr().iloc[:-1,-1:]
 relations = relations.sort_values(by=['class'],ascending=False)
 X = X[relations[relations['class'] > 0.09].index]
+#########################################################
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
