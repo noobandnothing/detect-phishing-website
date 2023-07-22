77fef9837b0dc5b1ca4f5585fdab7985359084b3
noobandnothing <popcorn.server.project@gmail.com>
2023-07-22 13:07:16 +0300

    testdomain_object : add IP and Valid - Get ip address of domain - Check if it is already exist

--- testdomain.py
+++ testdomain.py
@@ -1,4 +1,5 @@
 import tldextract
+import socket
 from urllib.parse import urlparse
 
 
@@ -11,14 +12,37 @@ class testdomain:
             raise Exception("Sorry, Invalid URL")
         self.subdomain, self.domain , self.tld = tldextract.extract(url)
         self.domain = self.domain +"."+ self.tld
+        self.ip = self.__getip()
+        self.valid = self.__IsValid()
+        if not self.valid:
+            print("Lets go out")            
+        else:
+            print("Lets continue")
         self.info()
+
+    def __getip(self):
+        try:
+            return socket.gethostbyname(self.domain)
+        except:
+            # Domain name is not found
+            return -1
     
+    def __IsValid(self):
+        try:
+            socket.inet_aton(self.ip)
+            return True
+        except:
+            # Domain name is not found
+            return False
+        
     def info(self):
         print("url is : " +str(self.url))
         print("length is : " +str(self.length))
         print("protocol is : " +str(self.protocol))
         print("subdomain is : " +str(self.subdomain))
         print("domain is : " +str(self.domain))
+        print("IP is : " +str(self.ip))
+        print("Valid is : " +str(self.valid))
 
 
-v = testdomain("https://wiki.eclipse.org/ToString()_generation")
+v = testdomain("https://wikdafsasfasfi.eclipse.org/ToString()_generation")
