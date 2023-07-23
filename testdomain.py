649ea9caf45bb1954a0999c5290ed3d86499910b
noobandnothing <popcorn.server.project@gmail.com>
2023-07-24 01:42:19 +0300

    testdomain_object : Make big improvement & fix many bugs & finish object
    - use headless
    - remove redundant part and use better way
    - fix bugs and improve exceptions
    - Auto create data depends on model
    - others improvements .......

--- testdomain.py
+++ testdomain.py
@@ -4,13 +4,10 @@ import datetime
 import whois
 import requests
 import time
-
-
+        
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 class testdomain:
     def __init__(self,url):
         self.data = []
@@ -30,28 +27,39 @@ class testdomain:
                 self.data.append(1)          
         else:
             self.IsDomain = (self.ip != self.domain)
-            self.age = self.__getage()
-            self.expire = self.__get_domain_expiration()
-            self.google_index = self.__get_googleindex()
-            ###############            ###############    ###############
-            self.https = self.__check_https()
-            self.url_anchor = self.__get_url_anchor()
-            self.url_prefixandsuffix = self.__get_url_prefixandsuffix()
-            tmp = self.__get_webtraffic_pagerank()
-            self.pagerank = tmp[0]
-            self.webtraffic = tmp[1]
-            self.requesturl =  self.__get_requesturl()
-            self.urlmeta = self.__get_url_meta()
-            self.sfh = self.__get_SFH()
+            #self.expire = self.__get_domain_expiration()
+            # ###############            ###############    ###############
+            tmp = self.__getsomeinfo()
+            if type(tmp) == type(1) :
+                raise Exception("Sorry, there is problem "+str(tmp))
+            else:
+                self.obj_soup , self.resp_url = self.__getsomeinfo()
+            # ###############            ###############    ###############
+            self.HTTPS = self.__check_https()
+            self.AnchorURL = self.__get_url_anchor()
+            self.PrefixSuffix = self.__get_url_prefixandsuffix()
+            self.PageRank,self.WebsiteTraffic = self.__get_webtraffic_pagerank()
+            # ###############           ###############    ################
+            if len(self.subdomain.split(".")) == 1:
+                self.SubDomains = -1
+            elif len(self.subdomain.split(".")) == 2:
+                self.SubDomains = 0
+            else:
+                self.SubDomains = 1
+            # ###############           ###############    ################
+            self.RequestURL =  self.__get_requesturl()
+            self.LinksInScriptTags = self.__get_url_meta()
+            self.ServerFormHandler = self.__get_SFH()
+            self.GoogleIndex = self.__get_googleindex()
+            self.AgeofDomain = self.__getage()
+            # ###############           ###############    ################
+            if self.IsDomain:
+                self.UsingIP = -1
+            else:
+                self.UsingIP = 1
             
-            self.info()
-
-    def __getip(self):
-        try:
-            return socket.gethostbyname(self.domain)
-        except:
-            # Domain name is not found
-            return -1
+        #     ################################################################
+            self.__getbrief()
     
     def __IsValid(self):
         try:
@@ -63,7 +71,11 @@ class testdomain:
     
     def __getage(self):
         try:
-            return int((datetime.datetime.now() - whois.whois(self.domain).creation_date[0]).days)
+            age =  int((datetime.datetime.now() - whois.whois(self.domain).creation_date[0]).days)
+            if age >= 6 :
+                return -1
+            else:
+                return 1
         except:
             return -1
     
@@ -81,37 +93,72 @@ class testdomain:
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
         response = requests.get(f'{"https://www.google.com/search?q="}'+f'{self.domain}', headers=headers)
         soup = BeautifulSoup(response.content, "html.parser")
-        return int(soup.select_one("#result-stats").text.strip().split(' ')[1].replace(",",""))
+        response.close()
+        tmp = int(soup.select_one("#result-stats").text.strip().split(' ')[1].replace(",",""))
+        if tmp :
+            return -1
+        else:
+            return 1
+    
+    def __getsomeinfo(self):
+        #GSI
+        tmp = []
+        try:
+            # response = requests.post(self.url)
+            # tmp.append(BeautifulSoup(response.text, 'html.parser'))
+            # tmp.append(response.url)
+            # response.close()
+            # Use this way to prevent blocking robot
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless=new')
+            options.add_argument('--disable-gpu') 
+            driver = webdriver.Chrome(options = options)
+            driver.get(self.url)
+            time.sleep(5)
+            html = driver.page_source
+            tmp.append(BeautifulSoup(html, 'html.parser'))
+            tmp.append(driver.current_url)
+            driver.quit()
+            return tmp
+        except Exception as err: 
+            if 'InvalidSchema' in str(type(err)):
+                # it redirect to none or unkown dest
+                return 0
+            elif 'ConnectionError' in str(type(err)) or 'SSLError' in str(type(err)):
+                # Connection error
+                return 1
+            else:
+                raise Exception("Error GSI: " +str(err))
+            
 
     def __check_https(self):
         if self.protocol != "https":
             return 1
         else:
             try:
-                response = requests.head(self.url)
-                return int(response.url.startswith("https://"))*-1
-            # There is more to check cert
+                return int(self.resp_url.startswith("https://"))*-1 
+            # There are more to check cert
             except requests.exceptions.RequestException:
                 return 1
 
+    def __getip(self):
+        try:
+            return socket.gethostbyname(self.domain)
+        except:
+            # Domain name is not found
+            return -1
+        
     # GET anchor######################
     
     # Function to calculate the percentage of anchor tags that do not link to any webpage
     def __get_url_anchor(self):
-        # LINUX DRIVER
-        #geckodriver_path = ' '  # Replace with the actual path
-        #driver = webdriver.Firefox(executable_path=geckodriver_path)
-        driver = webdriver.Chrome(options = webdriver.ChromeOptions())
-        driver.get(self.url)
-        time.sleep(5)
-        html = driver.page_source
-        driver.quit()
-        soup = BeautifulSoup(html, 'html.parser')
+        #import copy
+        #soup = copy.deepcopy(self.obj_soup)
 
         total_anchors = 0
         no_link_anchors = 0
         
-        for tag in soup.find_all('a'):
+        for tag in self.obj_soup.find_all('a'):
             total_anchors += 1
 
             anchor_url = tag.get('href')
@@ -135,77 +182,82 @@ class testdomain:
             return -1
         
     ##################################
-    
     def __get_webtraffic_pagerank(self):
-        # LINUX DRIVER
-        #geckodriver_path = ' '  # Replace with the actual path
-        #driver = webdriver.Firefox(executable_path=geckodriver_path)
-        driver = webdriver.Chrome(options = webdriver.ChromeOptions())
-        if self.subdomain:
-            target = 'https://www.similarweb.com/website/'+self.subdomain+"."+self.domain+'/#overview'
-        else:
-            target = 'https://www.similarweb.com/website/'+self.domain+'/#overview'
-        driver.get(target)
-        time.sleep(10)
-        current_url = driver.current_url
-        if current_url != target:
+        #GWTPR
+        try:
+            # LINUX DRIVER
+            #geckodriver_path = ' '  # Replace with the actual path
+            #driver = webdriver.Firefox(executable_path=geckodriver_path)
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless=new')
+            options.add_argument('--disable-gpu') 
+            driver = webdriver.Chrome(options = options)
+            if self.subdomain:
+                target = 'https://www.similarweb.com/website/'+self.subdomain+"."+self.domain+'/#overview'
+            else:
+                target = 'https://www.similarweb.com/website/'+self.domain+'/#overview'
+            driver.get(target)
+            time.sleep(10)
+            current_url = driver.current_url
+            if current_url != target:
+                driver.quit()
+                return [1,1]
+            html = driver.page_source
+
             driver.quit()
-            return [1,1]
-        html = driver.page_source
 
-        driver.quit()
-        from bs4 import BeautifulSoup
-        soup = BeautifulSoup(html,'html.parser')
-        if len(soup.find_all('div', class_='search-results__no-data')) == 1 :
-            return [1,1]
-        another = BeautifulSoup(str(soup.find_all('div', class_='engagement-list__item')),'html.parser')
-        x = str(another.find_all('p', class_='engagement-list__item-value')[0])
-        x = x.split('>')[1].split('<')[0].replace("'","").replace(",","").replace("-","")
-        if "B" in x:
-            x = x.replace("B","")
-            x = float(x)
-            x = x * 1000000000
-        elif "M" in x:
-            x = x.replace("M","")
-            x = float(x)
-            x = x * 1000000
-        elif "K" in x:
-            x = x.replace("K","")
-            x = float(x)
-            x = x * 1000
+            soup = BeautifulSoup(html,'html.parser')
 
-        webtraffic = 0
-        if  x == ' ':
-            return [1,1]
-        else:
-            webtraffic = float(x)
-        
-            page_rank = str(soup.find_all('p', class_='wa-rank-list__value')[0]).split('>')[-2].split('<')[0].replace("'","").replace(",","").replace("-","")
-            if page_rank == " ":
-                page_rank = 0
+            if len(soup.find_all('div', class_='search-results__no-data')) == 1 :
+                return [1,1]
+            another = BeautifulSoup(str(soup.find_all('div', class_='engagement-list__item')),'html.parser')
+            x = str(another.find_all('p', class_='engagement-list__item-value')[0])
+            x = x.split('>')[1].split('<')[0].replace("'","").replace(",","").replace("-","")
+            if "B" in x:
+                x = x.replace("B","")
+                x = float(x)
+                x = x * 1000000000
+            elif "M" in x:
+                x = x.replace("M","")
+                x = float(x)
+                x = x * 1000000
+            elif "K" in x:
+                x = x.replace("K","")
+                x = float(x)
+                x = x * 1000
+
+            webtraffic = 0
+            if  x == ' ':
+                return [1,1]
             else:
-                page_rank = float(page_rank)
+                webtraffic = float(x)
+            
+                page_rank = str(soup.find_all('p', class_='wa-rank-list__value')[0]).split('>')[-2].split('<')[0].replace("'","").replace(",","").replace("-","")
+                if page_rank == " ":
+                    page_rank = 0
+                else:
+                    page_rank = float(page_rank)
 
-            arr = []
-            if page_rank == 0:
-                arr.append(1)
-            else:
-                arr.append(-1)
+                arr = []
+                if page_rank == 0:
+                    arr.append(1)
+                else:
+                    arr.append(-1)
 
-            if webtraffic < 100000:
-                arr.append(1)
-            else:
-                arr.append(-1)
-        
-            return arr[0],arr[1]
-    ##################################
+                if webtraffic < 100000:
+                    arr.append(1)
+                else:
+                    arr.append(-1)
+            
+                return arr[0],arr[1]
+        except Exception as err: 
+            raise Exception("Error GWTPR: Please try again " +str(err))
+    ##########HERE####HERE############
     def __get_requesturl(self):
         total_links = 0
         links_in_domain = 0
         try:
-            response = requests.get(self.url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            for tag in soup.find_all(['img', 'source']):
+            for tag in self.obj_soup.find_all(['img', 'source']):
                 if tag.get('src'):
                     link = tag.get('src')
                     if str(urlparse(link).scheme) == '' or tldextract.extract(link)[1]  is None:
@@ -236,21 +288,11 @@ class testdomain:
         return -1
     ##################################
     def __get_url_meta(self):
-        # LINUX DRIVER
-        #geckodriver_path = ' '  # Replace with the actual path
-        #driver = webdriver.Firefox(executable_path=geckodriver_path)
-        driver = webdriver.Chrome(options = webdriver.ChromeOptions())
-        driver.get(self.url)
-        time.sleep(5)
-        html = driver.page_source
-        driver.quit()
-        soup = BeautifulSoup(html, 'html.parser')
-        
         total_links = 0
         links_in_tags = 0
 
         # Count total links and links in <Meta>, <Script>, and <Link> tags
-        for tag in soup.find_all(['meta', 'script', 'link']):
+        for tag in self.obj_soup.find_all(['meta', 'script', 'link']):
             if tag.get('href') or tag.get('src'):
                 total_links += 1
                 if tag.name in ['meta', 'script', 'link'] and tag.get('href') and tag.get('href').startswith('http'):
@@ -267,9 +309,7 @@ class testdomain:
     ##################################
     def __get_SFH(self):
         try:
-            response = requests.post(self.url)
-            sfh = response.url
-            sfh_subdomain, sfh_domain , sfh_tld = tldextract.extract(sfh)
+            sfh_subdomain, sfh_domain , sfh_tld = tldextract.extract(self.resp_url)
             if self.IsDomain:
                 if sfh_domain+"."+sfh_tld == self.domain:
                     return -1
@@ -312,26 +352,18 @@ class testdomain:
     #         return None
     ##################################
 
-    def info(self):
-        print("url is : " +str(self.url))
-        print("length is : " +str(self.length))
-        print("protocol is : " +str(self.protocol))
-        print("subdomain is : " +str(self.subdomain))
-        print("domain is : " +str(self.domain))
-        print("IP is : " +str(self.ip))
-        print("Valid is : " +str(self.valid))
-        print("IsDomain is : " +str(self.IsDomain))
-        print("Age is : " +str(self.age))
-        print("Expire is : " +str(self.expire))
-        print("Google Index is : " +str(self.google_index))
-        print("HTTPS is : " +str(self.https))
-        print("URL Anchor is : " +str(self.url_anchor))
-        print("URL Prefix-Suffix is : " +str(self.url_prefixandsuffix))
-        print("Page Rank is : " +str(self.pagerank))
-        print("Web Traffic is : " +str(self.webtraffic))
-        print("Request url is : " +str(self.requesturl))
-        print("Url Meta is : " +str(self.urlmeta))
-        print("SFH is : " +str(self.sfh))
+    def __getbrief(self):
+        import os
+        if os.path.isfile('DTM.pkl'):
+            from joblib import load
+            clf = load('DTM.pkl')
+            for attribute_name in clf.feature_names_in_:
+                self.data.append(getattr(self, attribute_name.replace("-","")))
+        else:
+            return None
+            
 
 
-v = testdomain("https://127.0.0.1/mytest/x.php")
+
+#test = testdomain("https://google.com")
+
